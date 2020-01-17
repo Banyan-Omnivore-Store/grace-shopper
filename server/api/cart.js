eb22193f5419b8595047bce24b56b45681e93368
Nicky Spiva <nick.spiva@gmail.com>
2020-01-17 12:04:23 -0600

    set up basic guest cart functionality can add to the guest cart

--- server/api/cart.js
+++ server/api/cart.js
@@ -1,73 +1,135 @@
+/* eslint-disable max-statements */
+/* eslint-disable complexity */
 const router = require('express').Router()
 const {Op} = require('sequelize')
 const {Order, Product, OrderItem} = require('../db/models')
 module.exports = router
 
 router.get('/', async (req, res, next) => {
-  try {
-    const cart = await Order.findOrCreate({
-      where: {
-        userId: req.user.id,
-        status: {
-          [Op.or]: ['cartEmpty', 'cartNotEmpty']
-        }
-      },
-      defaults: {
-        status: 'cartEmpty'
-      },
-      include: [{model: Product}]
-    })
-    res.json(cart[0])
-  } catch (err) {
-    next(err)
+  if (!req.user) {
+    if (!req.session.cart) {
+      req.session.cart = {products: []}
+    }
+    res.send(req.session.cart)
+  } else {
+    console.log('user req.session: ', req.session)
+    try {
+      const cart = await Order.findOrCreate({
+        where: {
+          userId: req.user.id,
+          status: {
+            [Op.or]: ['cartEmpty', 'cartNotEmpty']
+          }
+        },
+        defaults: {
+          status: 'cartEmpty'
+        },
+        include: [{model: Product}]
+      })
+      res.json(cart[0])
+    } catch (err) {
+      next(err)
+    }
   }
 })
 
 router.put('/order/:orderId', async (req, res, next) => {
-  try {
-    const order = await Order.findOne({
-      where: {id: req.params.orderId},
-      status: {
-        [Op.or]: ['cartEmpty', 'cartNotEmpty']
-      }
-    })
-    const prevQty = await OrderItem.findOne({
-      where: {orderId: req.params.orderId, productId: req.body.productId}
-    })
-
-    const productId = req.body.productId
-    const quantity = req.body.quantity
-    const product = await Product.findOne({
-      where: {id: productId}
-    })
-
-    if (prevQty) {
-      if (parseInt(quantity, 10) + prevQty.quantity > product.quantity) {
-        res.send('item not added to cart, not enough inventory')
-        alert('not enough inventory')
-      }
-      await order.addProduct(product, {
-        through: {
-          quantity: parseInt(quantity, 10) + prevQty.quantity
-        }
+  if (!req.user) {
+    try {
+      const productId = req.body.productId
+      const quantity = req.body.quantity
+      //   const prevQty = await OrderItem.findOne({
+      //     where: {orderId: req.params.orderId, productId: req.body.productId}
+      //   })
+      const product = await Product.findOne({
+        where: {id: productId}
       })
-    } else {
+      //   if (false) {
+      //     if (parseInt(quantity, 10) + prevQty.quantity > product.quantity) {
+      //       res.send('item not added to cart, not enough inventory')
+      //       alert('not enough inventory')
+      //     }
+      //     await order.addProduct(product, {
+      //       through: {
+      //         quantity: parseInt(quantity, 10) + prevQty.quantity
+      //       }
+      //     })
+      //   } else
       if (quantity > product.quantity) {
         res.send('item not added to cart, not enough inventory')
         alert('not enough inventory')
+      } else {
+        console.log('REQ SESSION!!!!', req.session)
+        let index = req.session.cart.products.reduce(
+          (finalIndex, item, currentIndex) => {
+            if (item.product.id === productId) {
+              finalIndex = currentIndex
+            }
+            return finalIndex
+          },
+          -1
+        )
+        console.log('INDEX OF FOUND ITEM!!!:', index)
+        if (index === -1) {
+          req.session.cart.products.push({
+            product: product,
+            quantity: quantity
+          })
+        } else {
+          req.session.cart.products[index].quantity =
+            quantity + req.session.cart.products[index].quantity
+        }
+        res.send('item added to cart')
       }
-      await order.addProduct(product, {
-        through: {
-          quantity: quantity
+    } catch (err) {
+      next(err)
+    }
+  } else {
+    try {
+      const order = await Order.findOne({
+        where: {id: req.params.orderId},
+        status: {
+          [Op.or]: ['cartEmpty', 'cartNotEmpty']
         }
       })
-      if (order.status === 'cartEmpty') {
-        await order.update({status: 'cartNotEmpty'})
+      const prevQty = await OrderItem.findOne({
+        where: {orderId: req.params.orderId, productId: req.body.productId}
+      })
+
+      const productId = req.body.productId
+      const quantity = req.body.quantity
+      const product = await Product.findOne({
+        where: {id: productId}
+      })
+
+      if (prevQty) {
+        if (parseInt(quantity, 10) + prevQty.quantity > product.quantity) {
+          res.send('item not added to cart, not enough inventory')
+          alert('not enough inventory')
+        }
+        await order.addProduct(product, {
+          through: {
+            quantity: parseInt(quantity, 10) + prevQty.quantity
+          }
+        })
+      } else {
+        if (quantity > product.quantity) {
+          res.send('item not added to cart, not enough inventory')
+          alert('not enough inventory')
+        }
+        await order.addProduct(product, {
+          through: {
+            quantity: quantity
+          }
+        })
+        if (order.status === 'cartEmpty') {
+          await order.update({status: 'cartNotEmpty'})
+        }
       }
+      res.send('item added to cart')
+    } catch (err) {
+      next(err)
     }
-    res.send('item added to cart')
-  } catch (err) {
-    next(err)
   }
 })
 
